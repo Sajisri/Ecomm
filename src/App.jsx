a8c55fd1a865e9699889b74bd6c186d417eb68a4
sajisri <sajisrikumar@gmail.com>
2025-08-26 02:50:23 +0530

    fix vite base path

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./Pages/Home";
 import Product from "./Pages/Product";
